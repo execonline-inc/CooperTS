1a95c9eabf92e6d673fda0967f665bb06224ea30
Ryan L. Bell <ryan.l.bell@gmail.com>
2020-02-04 11:34:37 -0500

    Fix concatAll for empty arrays

--- packages/collections/src/concat.ts
+++ packages/collections/src/concat.ts
@@ -1,5 +1,7 @@
 export const concat = <A>(as1: A[]) => (as2: A[]): A[] => as1.concat(as2);
 
-export const concatAll = <T>(ary: ReadonlyArray<T[]>): ReadonlyArray<T> => {
-  return ary.reduce((memo, a) => [...memo, ...a]);
+export const concatAll = <T>(
+  ary: ReadonlyArray<readonly T[]>
+): ReadonlyArray<T> => {
+  return ary.reduce((memo, a) => [...memo, ...a], []);
 };
