a3ecc6f62a549be72a5086b96394f802f109ad8b
Ryan L. Bell <ryan.l.bell@gmail.com>
2020-12-08 15:09:32 -0500

    Add functions for updating query params safely

--- packages/url/src/url.ts
+++ packages/url/src/url.ts
@@ -1,17 +1,21 @@
-import { noop } from '@kofno/piper';
-import { just, Maybe, nothing } from 'maybeasy';
+import { always, identity, noop } from '@kofno/piper';
+import { fromNullable, just, Maybe, nothing } from 'maybeasy';
 import { err, ok, Result } from 'resulty';
 import Task from 'taskarian';
-import * as URL from 'url-parse';
+import * as ParsedURL from 'url-parse';
 
 export interface InvalidUrlError {
   kind: 'invalid-url-error';
   href: string;
-  base?: string | URL;
+  base?: string | ParsedURL;
   error: unknown;
 }
 
-const invalidUrlError = (error: unknown, href: string, base?: string | URL): InvalidUrlError => {
+const invalidUrlError = (
+  error: unknown,
+  href: string,
+  base?: string | ParsedURL
+): InvalidUrlError => {
   return {
     kind: 'invalid-url-error',
     href,
@@ -20,24 +24,27 @@ const invalidUrlError = (error: unknown, href: string, base?: string | URL): Inv
   };
 };
 
-export const toUrlR = (href: string, base?: string | URL): Result<InvalidUrlError, URL> => {
+export const toUrlR = (
+  href: string,
+  base?: string | ParsedURL
+): Result<InvalidUrlError, ParsedURL> => {
   try {
     // Working around a bug in safari: https://github.com/zloirock/core-js/issues/656
-    const url = base ? new URL(href, base) : new URL(href);
+    const url = base ? new ParsedURL(href, base, true) : new ParsedURL(href, true);
     return ok(url);
   } catch (e) {
     return err(invalidUrlError(e, href, base));
   }
 };
 
-export const toUrl = (href: string, base?: string | URL): Maybe<URL> => {
-  return toUrlR(href, base).cata<Maybe<URL>>({
+export const toUrl = (href: string, base?: string | ParsedURL): Maybe<ParsedURL> => {
+  return toUrlR(href, base).cata<Maybe<ParsedURL>>({
     Ok: just,
     Err: nothing,
   });
 };
 
-export const toUrlT = (href: string, base?: string | URL): Task<InvalidUrlError, URL> =>
+export const toUrlT = (href: string, base?: string | ParsedURL): Task<InvalidUrlError, ParsedURL> =>
   new Task((reject, resolve) => {
     toUrlR(href, base).cata({
       Ok: resolve,
@@ -46,3 +53,27 @@ export const toUrlT = (href: string, base?: string | URL): Task<InvalidUrlError,
 
     return noop;
   });
+
+export function getQueryParam(key: string): (url: ParsedURL) => Maybe<string>;
+export function getQueryParam(key: string, url: ParsedURL): Maybe<string>;
+export function getQueryParam(key: string, url?: ParsedURL) {
+  const getIt = (url: ParsedURL) => fromNullable(url.query[key]);
+
+  return url ? getIt(url) : getIt;
+}
+
+export function putQueryParam(key: string, value: string): (url: ParsedURL) => ParsedURL;
+export function putQueryParam(key: string, value: string, url: ParsedURL): ParsedURL;
+export function putQueryParam(key: string, value: string, url?: ParsedURL) {
+  const putIt = (url: ParsedURL): ParsedURL => {
+    const q = url.query;
+    return toUrl(url.href)
+      .map(u => u.set('query', { ...q, [key]: value }))
+      .cata({
+        Just: identity,
+        Nothing: always(url),
+      });
+  };
+
+  return url ? putIt(url) : putIt;
+}
