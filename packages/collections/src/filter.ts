538276f9f6a2058e0af63398293f36c5035443b4
Ryan L. Bell <ryan.l.bell@gmail.com>
2021-09-10 09:28:23 -0400

    Add filter function

--- packages/collections/src/filter.ts
+++ packages/collections/src/filter.ts
@@ -1,8 +1,6 @@
-import { identity } from "@kofno/piper";
+import { identity } from '@kofno/piper';
 
-export const uniqBy = <T, C>(fn: (t: T) => C) => (
-  ts: ReadonlyArray<T>
-): T[] => {
+export const uniqBy = <T, C>(fn: (t: T) => C) => (ts: ReadonlyArray<T>): T[] => {
   const set: Set<C> = new Set();
   const result: T[] = [];
 
@@ -18,3 +16,11 @@ export const uniqBy = <T, C>(fn: (t: T) => C) => (
 };
 
 export const uniq: <T>(ts: ReadonlyArray<T>) => T[] = uniqBy(identity);
+
+export function filter<T>(fn: (t: T) => boolean): (ts: ReadonlyArray<T>) => ReadonlyArray<T>;
+export function filter<T>(fn: (t: T) => boolean, ts: ReadonlyArray<T>): ReadonlyArray<T>;
+export function filter<T>(fn: (t: T) => boolean, ts?: ReadonlyArray<T>) {
+  const doit = (ts: ReadonlyArray<T>): ReadonlyArray<T> => ts.filter(fn);
+
+  return typeof ts === 'undefined' ? doit : doit(ts);
+}
