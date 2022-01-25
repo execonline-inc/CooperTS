7dd127c944af2c6ab7bd73d2639d9273a2fd3fc9
Ryan L. Bell <ryan.l.bell@gmail.com>
2022-01-25 13:12:41 -0500

    collections: Curry bifurcateWhen

--- packages/collections/src/bifurcation.ts
+++ packages/collections/src/bifurcation.ts
@@ -1,25 +1,32 @@
-export const bifurcateWhen = <T>(
+export function bifurcateWhen<T>(
+  delim: (t: T) => boolean
+): (ts: ReadonlyArray<T>) => [ReadonlyArray<T>, ReadonlyArray<T>];
+export function bifurcateWhen<T>(
   delim: (t: T) => boolean,
   ts: ReadonlyArray<T>
-): [ReadonlyArray<T>, ReadonlyArray<T>] => {
+): [ReadonlyArray<T>, ReadonlyArray<T>];
+export function bifurcateWhen<T>(delim: (t: T) => boolean, ts?: ReadonlyArray<T>) {
   interface Tuple {
     found: boolean;
     head: ReadonlyArray<T>;
     tail: ReadonlyArray<T>;
   }
-  const reducer = (memo: Tuple, v: T): Tuple => {
-    if (memo.found) {
-      return { ...memo, tail: [...memo.tail, v] };
-    } else if (delim(v)) {
-      return { ...memo, found: true };
-    } else {
-      return { ...memo, head: [...memo.head, v] };
-    }
+  const doit = (ts: ReadonlyArray<T>) => {
+    const reducer = (memo: Tuple, v: T): Tuple => {
+      if (memo.found) {
+        return { ...memo, tail: [...memo.tail, v] };
+      } else if (delim(v)) {
+        return { ...memo, found: true };
+      } else {
+        return { ...memo, head: [...memo.head, v] };
+      }
+    };
+    const { head, tail } = ts.reduce(reducer, {
+      found: false,
+      head: [],
+      tail: [],
+    });
+    return [head, tail];
   };
-  const { head, tail } = ts.reduce(reducer, {
-    found: false,
-    head: [],
-    tail: []
-  });
-  return [head, tail];
-};
+  return typeof ts === 'undefined' ? doit : doit(ts);
+}
