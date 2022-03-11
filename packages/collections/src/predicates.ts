d109eb4aa0ad717a415e2c4bfd24eb22e6524902
Ryan L. Bell <ryan.l.bell@gmail.com>
2022-03-11 13:32:59 -0500

    collections: Adds equals predicate

--- packages/collections/src/predicates.ts
+++ packages/collections/src/predicates.ts
@@ -1,10 +1,17 @@
-export const byId = (id: number) => (thing: { id: number }): boolean =>
-  id === thing.id;
+export const byId = (id: number) => (thing: { id: number }): boolean => id === thing.id;
 
-export const byPayloadId = <T extends { payload: { id: number } }>(
-  id: number
-) => (thing: T): boolean => id === thing.payload.id;
+export const byPayloadId = <T extends { payload: { id: number } }>(id: number) => (
+  thing: T
+): boolean => id === thing.payload.id;
 
-export const byPayloadUuid = <T extends { payload: { uuid: string } }>(
-  uuid: string
-) => (thing: T): boolean => uuid === thing.payload.uuid;
+export const byPayloadUuid = <T extends { payload: { uuid: string } }>(uuid: string) => (
+  thing: T
+): boolean => uuid === thing.payload.uuid;
+
+export function equals<T>(left: T): (right: T) => boolean;
+export function equals<T>(left: T, right: T): boolean;
+export function equals<T>(left: T, right?: T) {
+  const doit = (right: T): boolean => left === right;
+
+  return typeof right === 'undefined' ? doit : doit(right);
+}
