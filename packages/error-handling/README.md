08d3e28e556c94bf3558ec54ad55f0ea1459d3b5
kofno <ryan.l.bell@gmail.com>
2022-05-23 11:40:04 -0400

    error-handling: Added error handling library

---
+++ packages/error-handling/README.md
@@ -0,0 +1,11 @@
+# `error-handling`
+
+> TODO: description
+
+## Usage
+
+```
+const errorHandling = require('error-handling');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/error-handling/src/error-handling.ts
@@ -0,0 +1,22 @@
+import { just, Maybe, nothing } from 'maybeasy';
+import { err, ok, Result } from 'resulty';
+import Task from 'taskarian';
+
+export const asTask = <E, T>(result: Result<E, T>): Task<E, T> =>
+  result.cata<Task<E, T>>({ Ok: Task.succeed, Err: Task.fail });
+
+export const asMaybe = <E, T>(result: Result<E, T>): Maybe<T> =>
+  result.cata<Maybe<T>>({ Ok: just, Err: nothing });
+
+export const runResult = <T>(throwable: () => T): Result<unknown, T> => {
+  try {
+    const value = throwable();
+    return ok(value);
+  } catch (e) {
+    return err(e);
+  }
+};
+
+export const runMaybe = <T>(throwable: () => T): Maybe<T> => asMaybe(runResult(throwable));
+
+export const runTask = <T>(throwable: () => T): Task<unknown, T> => asTask(runResult(throwable));
