24b2d9468ba708df787bdf423266106cfbc426dc
kofno <ryan.l.bell@gmail.com>
2022-10-12 11:46:19 -0400

    string: add capitalize implementation

--- packages/strings/__tests__/strings.test.js
+++ packages/strings/__tests__/strings.test.js
@@ -3,5 +3,9 @@
 const strings = require('..');
 const assert = require('assert').strict;
 
-assert.strictEqual(strings(), 'Hello from strings');
-console.info("strings tests passed");
+assert.strictEqual(strings.capitalize('foo'), 'Foo');
+assert.strictEqual(strings.capitalize('italya', 'tr'), 'İtalya');
+assert.strictEqual(strings.capitalize('𐐶𐐲𐑌𐐼𐐲𐑉'), '𐐎𐐲𐑌𐐼𐐲𐑉');
+assert.strictEqual(strings.capitalize(''), '');
+
+console.info('strings tests passed');
--- packages/strings/src/strings.ts
+++ packages/strings/src/strings.ts
@@ -1,3 +1,6 @@
+import { pipe } from '@kofno/piper';
+import { fromNullable } from 'maybeasy';
+
 export function truncate(length: number, str: string): string;
 export function truncate(length: number): (str: string) => string;
 export function truncate(length: number, str?: string) {
@@ -6,4 +9,20 @@ export function truncate(length: number, str?: string) {
   const doit = (str: string) =>
     str.length > length ? `${str.substring(0, endIndex)}${elipses}` : str;
   return typeof str === 'undefined' ? doit : doit(str);
+}
+
+function multibyteCodePoint(code: number): boolean {
+  return code > 0xffff;
+}
+
+const codePointIndex = pipe(multibyteCodePoint, (multi: boolean) => (multi ? 2 : 1));
+
+export function capitalize(value: string, language?: string | string[]): string {
+  return fromNullable(value.codePointAt(0))
+    .map(codePoint => {
+      const upper = String.fromCodePoint(codePoint).toLocaleUpperCase(language);
+      const rest = value.substring(codePointIndex(codePoint));
+      return upper + rest;
+    })
+    .getOrElseValue(value);
 }
