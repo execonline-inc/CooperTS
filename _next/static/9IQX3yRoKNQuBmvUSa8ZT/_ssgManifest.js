5a3bc731ad6a95e130e35187131dffe44bd823b4
spejamchr <jc.spencer92@gmail.com>
2022-11-17 18:10:38 +0000

    Deploying to gh-pages from @ execonline-inc/CooperTS@1c3e0a97130208069122f26df54f262bc77700ac 🚀

--- _next/static/9IQX3yRoKNQuBmvUSa8ZT/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F404","\u002F500","\u002Fabout","\u002Fguide","\u002F","\u002Fpackages","\u002Fguide\u002F[slug]","\u002Fpackages\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
