fc88540e16489426e5139a1200d2dad660a4359d
Spencer Christiansen <jc.spencer92@gmail.com>
2023-05-04 16:38:45 -0600

    Write content for the About page

--- docs/src/pages/about.tsx
+++ docs/src/pages/about.tsx
@@ -1,20 +1,29 @@
 import { pipe } from '@kofno/piper';
 import { GetStaticProps, NextPage } from 'next';
 import Task from 'taskarian';
+import Markdown from '../components/Markdown';
 import PageTitle from '../components/PageTitle';
+import { ReadFileError, readFileT } from '../GetPackages/filesystem';
+import { SafeMarkdown, unsafeMarkdownFromContent } from '../Types/guide';
 import { taskToStaticProps, WithNavTree, withNavTreeStaticProp } from '../Types/NavTree';
 
-interface Props {}
+interface Props {
+  markdown: SafeMarkdown;
+}
 
-const About: NextPage<Props> = () => (
+const About: NextPage<Props> = ({ markdown }) => (
   <>
     <PageTitle title="About" />
-    <div>About</div>
+    <Markdown markdown={markdown} />
   </>
 );
 
+const getMarkdown = (): Task<ReadFileError, SafeMarkdown> =>
+  // This is safe because we're reading from markdown we own locally
+  readFileT('./src/about.md').map(unsafeMarkdownFromContent);
+
 export const getStaticProps: GetStaticProps<WithNavTree<Props>> = pipe(
-  (_context) => Task.succeed({}),
+  (_context) => Task.succeed({}).assign('markdown', getMarkdown),
   withNavTreeStaticProp,
   taskToStaticProps
 );
