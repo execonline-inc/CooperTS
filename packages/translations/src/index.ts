c76cb8665d5022dabba2ad37bbf6c2a72da53f2e
kofno <ryan.l.bell@gmail.com>
2025-03-06 10:19:34 -0500

    Bump translations package version to 11.0.0 and export new localization utilities

--- packages/translations/src/index.ts
+++ packages/translations/src/index.ts
@@ -1,11 +1,13 @@
 import AlreadyTranslated from './AlreadyTranslated';
+import L from './L';
 import NotTranslated from './NotTranslated';
 import TranslationsContext from './TranslationsContext';
 import TranslationsLoader from './TranslationsLoader';
+import { localizer } from './localizations';
 
 export * from './adapters/i18next';
 export * from './decoders';
 export * from './language';
 export * from './translations';
 export * from './types';
-export { AlreadyTranslated, NotTranslated, TranslationsContext, TranslationsLoader };
+export { AlreadyTranslated, L, localizer, NotTranslated, TranslationsContext, TranslationsLoader };
